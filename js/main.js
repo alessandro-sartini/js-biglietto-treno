e8303435e7a499748671d58bd3e2e66133bf7615
alessandro-sartini <ale.sartini.98@gmail.com>
2025-01-09 12:59:15 +0100

    ADD:message

--- js/main.js
+++ js/main.js
@@ -50,16 +50,16 @@ let priceBase = 0.21;
 if (eta < 18) {
 
     let priceDiscount = (kmPercorsi * priceBase) - 20 / 100;
-    console.log(priceDiscount.toFixed(2))
+    console.log("Il prezzo del tuo biglietto è di ",priceDiscount.toFixed(2))
 
 } else if (eta >= 65) {
 
     priceDiscount = (kmPercorsi * priceBase) - 40 / 100;
-    console.log(priceDiscount.toFixed(2))
+    console.log("Il prezzo del tuo biglietto è di ",priceDiscount.toFixed(2))
 
 } else {
-    
+
     let price = kmPercorsi * priceBase
     
-    console.log(price.toFixed(2))
+    console.log("Il prezzo del tuo biglietto è di ",price.toFixed(2))
 }
